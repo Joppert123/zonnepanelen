a9e23e6d9675a9f794dfa1d5271a8d92090cd19e
Neireau <k.t.c.gravenhorst@me.com>
2018-11-09 13:22:52 +0100

    Removed args

--- Python/connect.py
+++ Python/connect.py
@@ -1,10 +1,8 @@
-import serial
+import serial, time
 
-ser = serial.Serial("COM3", 19200, timeout=2)
-write = ser.write(b'ping/0')
-write
-read = ser.readline()
-read
-
-# test = ser.read(100)
-# test
+# ser = serial.Serial("COM3", 19200, timeout=2)
+# write = ser.write(b'ping/0')
+# read = ser.readline()
+#
+# write
+# print(read)
